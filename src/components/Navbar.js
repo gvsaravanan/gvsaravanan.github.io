a3a73d89c861828f2bb196b06c0f1c502f6bea10
G-Saravanan <gautham.saravanan50@k12.leanderisd.org>
2021-06-22 17:41:11 -0500

    Update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,12 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { Button } from "./Button";
-import "./Navbar.css";
+import "./Navbar.css"
 
 function Navbar() {
     const [click, setClick] = useState(false);
-    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
-
-    const showButton = () => {
-        if(window.innerWidth <= 960) {
-            setButton(false);
-        } else {
-            setButton(true);
-        }
-    }
-
-    window.addEventListener('resize', showButton);
 
     return (
         <>
@@ -58,17 +46,7 @@ function Navbar() {
                                 Projects
                             </Link>
                         </li>
-                        <li className="nav-item">
-                            <Link
-                                to="/sign-up"
-                                className="nav-links-mobile"
-                                onClick={closeMobileMenu}
-                            >
-                                Sign up
-                            </Link>
-                        </li>
                     </ul>
-                    {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
                 </div>
             </nav>
         </>
