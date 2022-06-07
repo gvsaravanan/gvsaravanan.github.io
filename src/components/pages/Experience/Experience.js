0c03ebee10c7b08d3871b5ea722f807755e7d776
G-Saravanan <gautham.saravanan50@k12.leanderisd.org>
2022-06-07 16:57:59 -0500

    Fix issues with navbar and edit spacing on pages

--- src/components/pages/Experience/Experience.js
+++ src/components/pages/Experience/Experience.js
@@ -10,24 +10,92 @@ export default function Experience() {
                     <div className={style["exp-container"]}>
                         <div className={style["exp"]}>
                             <div>
-                                <h5 style={{textAlign: "left", display: "inline-block", width: "50%"}}>Software Developer Intern</h5>
-                                <h6 style={{textAlign: "right", display: "inline-block", width: "50%", fontStyle: "normal"}}>July 2021 - August 2021&nbsp;</h6>
+                                <h5
+                                    style={{
+                                        textAlign: "left",
+                                        display: "inline-block",
+                                        width: "50%",
+                                    }}
+                                >
+                                    Software Developer Intern
+                                </h5>
+                                <h6
+                                    style={{
+                                        textAlign: "right",
+                                        display: "inline-block",
+                                        width: "50%",
+                                        fontStyle: "normal",
+                                    }}
+                                >
+                                    July 2021 - August 2021&nbsp;
+                                </h6>
                             </div>
-                            <h6>&nbsp;Dev Launchers</h6>
-                            <ul>
-                                <li>Dev Launchers is a non-profit organization aimed at bridging opportunity gaps in the technology industry</li>
-                                <li>Collaborated with a team of senior developers to help maintain, design, and improve their platform website</li>
-                                <li>Created fully-functioning components that are now displayed on their website</li>
-                                <li>Programmed with ReactJS, JavaScript, HTML, CSS, and Git</li>
+                            <h6>
+                                &nbsp;Dev Launchers
+                                <a
+                                    href="https://devlaunchers.com/"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    style={{color: "black", fontSize: "0.85rem", paddingLeft: "8px"}}
+                                >
+                                    <i class="fas fa-external-link-alt"></i>
+                                </a>
+                            </h6>
+                            <ul className={style["description"]}>
+                                <li>
+                                    Dev Launchers is a non-profit organization
+                                    aimed at bridging opportunity gaps in the
+                                    technology industry
+                                </li>
+                                <li>
+                                    Collaborated with a team of senior
+                                    developers to help maintain, design, and
+                                    improve their platform website
+                                </li>
+                                <li>
+                                    Created fully-functioning components that
+                                    are now displayed on their website
+                                </li>
+                                <li>
+                                    Programmed with ReactJS, JavaScript, HTML,
+                                    CSS, and Git
+                                </li>
                             </ul>
                         </div>
                         <div className={style["exp"]}>
                             <div>
-                                <h5 style={{textAlign: "left", display: "inline-block", width: "50%"}}>Job Title</h5>
-                                <h6 style={{textAlign: "right", display: "inline-block", width: "50%", fontStyle: "normal"}}>Work Dates&nbsp;</h6>
+                                <h5
+                                    style={{
+                                        textAlign: "left",
+                                        display: "inline-block",
+                                        width: "50%",
+                                    }}
+                                >
+                                    Job Title
+                                </h5>
+                                <h6
+                                    style={{
+                                        textAlign: "right",
+                                        display: "inline-block",
+                                        width: "50%",
+                                        fontStyle: "normal",
+                                    }}
+                                >
+                                    Work Dates&nbsp;
+                                </h6>
                             </div>
-                            <h6>&nbsp;Company</h6>
-                            <ul>
+                            <h6>
+                                &nbsp;Company
+                                <a
+                                    href="https://www.google.com"
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    style={{color: "black", fontSize: "0.85rem", paddingLeft: "8px"}}
+                                >
+                                    <i class="fas fa-external-link-alt"></i>
+                                </a>
+                            </h6>
+                            <ul className={style["description"]}>
                                 <li>Description</li>
                             </ul>
                         </div>
